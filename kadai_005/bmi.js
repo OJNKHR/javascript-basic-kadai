f0fe112365594febc220401df710da34a43adb06
OJNKHR <wanwan900713@gmail.com>
2024-09-11 17:17:40 +0900

    kadai_005を修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,12 @@
-//変数へ宣言・値の代入
-let bmi = (68 / (1.7 * 1.7));
+//変数の宣言
+let weight;
+let height; 
+let bmi;
+
+//値の代入
+weight = 68;
+height = 1.7;
+bmi = (weight / (height * height));
 
 //コンソールへ出力
-console.log( bmi );
+console.log(bmi);
